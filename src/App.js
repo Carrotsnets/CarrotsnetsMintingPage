5f80ba66f4ee770d87a574cbc3886228e4877d9e
sungyeon1 <sungyeon0320@gmail.com>
2022-02-11 01:16:21 +0900

    designed minting page

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
-import logo from "./logo.svg";
+import logo from "./Carrotsnets_logo.png";
+import gif from "./Carrotpicking.gif";
 import "./App.css";
 
 function App() {
-	const kaikasLogin = async () => {
-		try {
-			const wallet = await window.klaytn.enable();
-		} catch (ex) {
-			console.log(ex);
-		}
-	};
-	const connect = () => {
-		if (typeof window.klaytn !== "undefined") {
-			// Kaikas user detected. You can now use the provider.
-			const provider = window["klaytn"];
-			kaikasLogin();
-		}
-	};
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learn React
-				</a>
-				<button className="main-mint-btn" onClick={connect}>
-					Mint
-				</button>
-			</header>
-		</div>
-	);
+  const kaikasLogin = async () => {
+    try {
+      const wallet = await window.klaytn.enable();
+    } catch (ex) {
+      console.log(ex);
+    }
+  };
+  const connect = () => {
+    if (typeof window.klaytn !== "undefined") {
+      // Kaikas user detected. You can now use the provider.
+      const provider = window["klaytn"];
+      kaikasLogin();
+    }
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="Carrotsnets-logo" alt="Carrotsnets" />
+        <p className="connected_wallet">
+          {" "}
+          Connected Wallet:
+          <span className="wallet_address">0xFcc....05e0</span>
+        </p>
+        <button className="mint-btn" onClick={connect}>
+          Mint
+        </button>
+        <img src={gif} className="Carrotpicking" />
+        <div className="ground"></div>
+      </header>
+    </div>
+  );
 }
 
 export default App;
